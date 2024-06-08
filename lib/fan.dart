5489fe330a08c1037e66825fc222d52b0e176aba
Stefan Herpich <s10h@posteo.de>
2024-06-08 13:05:28 +0200

    feat: add night mode

--- lib/fan.dart
+++ lib/fan.dart
@@ -55,78 +55,120 @@ class Fan extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          // To handle overflow if content is too large
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => sendCommand('ON_OFF'),
+                      child: const Text('ON / OFF'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () => sendCommand('FRONT_REAR'),
+                      child: const Text('Front / Rear'),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Fan Speed',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Wrap(
+                spacing: 10,
                 children: [
                   ElevatedButton(
-                    onPressed: () => sendCommand('ON_OFF'),
-                    child: const Text('ON / OFF'),
+                    onPressed: () => sendCommand('SPEED_1'),
+                    child: const Text('slowest'),
                   ),
-                  const SizedBox(width: 16),
                   ElevatedButton(
-                    onPressed: () => sendCommand('FRONT_REAR'),
-                    child: const Text('Front / Rear'),
+                    onPressed: () => sendCommand('SPEED_2'),
+                    child: const Text('slower'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => sendCommand('SPEED_3'),
+                    child: const Text('normal'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => sendCommand('SPEED_4'),
+                    child: const Text('fast'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => sendCommand('SPEED_5'),
+                    child: const Text('fastest'),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Fan Speed',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Wrap(
-              spacing: 10,
-              children: [
-                ElevatedButton(
-                  onPressed: () => sendCommand('SPEED_1'),
-                  child: const Text('slowest'),
+              const SizedBox(height: 20),
+              const Text(
+                'Timer',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Wrap(
+                spacing: 10,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => sendCommand('TIMER_1H'),
+                    child: const Text('1 hour'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => sendCommand('TIMER_4H'),
+                    child: const Text('4 hours'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => sendCommand('TIMER_8H'),
+                    child: const Text('8 hours'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey[900],
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('SPEED_2'),
-                  child: const Text('slower'),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Row(
+                      children: [
+                        Icon(Icons.nightlight_round, color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          'Night Modes',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Running with slow speed for five minutes every hour for 7 hours',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 10),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () => sendCommand('NIGHT_MODE'),
+                        child: const Text('Mode 1'),
+                      ),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('SPEED_3'),
-                  child: const Text('normal'),
-                ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('SPEED_4'),
-                  child: const Text('fast'),
-                ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('SPEED_5'),
-                  child: const Text('fastest'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Timer',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Wrap(
-              spacing: 10,
-              children: [
-                ElevatedButton(
-                  onPressed: () => sendCommand('TIMER_1H'),
-                  child: const Text('1 hour'),
-                ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('TIMER_4H'),
-                  child: const Text('4 hours'),
-                ),
-                ElevatedButton(
-                  onPressed: () => sendCommand('TIMER_8H'),
-                  child: const Text('8 hours'),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
